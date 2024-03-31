7916ff2620487f00a2ec37148d67660c8d16c4af
jhyhoss <mamanimedina926@gmail.com>
2024-03-31 11:55:18 -0500

    nuevo3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pagesinicio/inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent,
+  children:[
+          { path: 'login', component: LoginComponent},
+            ]
+
+
+    },
+
+
+
+];
